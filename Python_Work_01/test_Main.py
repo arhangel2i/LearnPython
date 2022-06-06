c29d717ad9e0e626d31080fb056c8cd797b30078
arhangel2i <arhangel2i@mail.ru>
2022-06-06 14:28:31 +0300

    update work_01

--- Python_Work_01/test_Main.py
+++ Python_Work_01/test_Main.py
@@ -4,7 +4,7 @@ def test_answer():
     assert inc(4) == 5
 
 def test_answer1():
-    assert inc(4) == 5
+    assert inc2(2) == 4
 
 def test_answer2():
-    assert inc(4) == 5
+    assert inc2(4) == 5
