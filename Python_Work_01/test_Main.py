517a9e99da01a6d42783ea63496df80de7a29d85
arhangel2i <arhangel2i@mail.ru>
2022-06-06 14:27:20 +0300

    update work_01

--- Python_Work_01/test_Main.py
+++ Python_Work_01/test_Main.py
@@ -2,5 +2,9 @@ from Main import *
 
 def test_answer():
     assert inc(4) == 5
-    assert inc2(2) == 4
-    assert inc2(3) == 4
+
+def test_answer1():
+    assert inc(4) == 5
+
+def test_answer2():
+    assert inc(4) == 5
